9f4296619a25ccdbef864db51783c362d74597b4
Manan Shah <105140673+Shahman0605@users.noreply.github.com>
2023-10-26 18:20:41 +0530

    Add files via upload

---
+++ Huggingface_CLIP_utils.py
@@ -0,0 +1,82 @@
+
+"""
+## Other Functions
+"""
+# %%
+# a function to determine whether a string is a URL or not
+def is_url(string):
+    try:
+        result = parse.urlparse(string)
+        return all([result.scheme, result.netloc, result.path])
+    except:
+        return False
+    
+# a function to load an image
+def load_image(image_path):
+    if is_url(image_path):
+        return Image.open(requests.get(image_path, stream=True).raw)
+    elif os.path.exists(image_path):
+        return Image.open(image_path)
+        
+# a function to perform inference
+def get_caption(model, image_processor, tokenizer, image_path):
+    image = load_image(image_path)
+    # preprocess the image
+    img = image_processor(image, return_tensors="pt").to(device)
+    # generate the caption (using greedy decoding by default)
+    output = model.generate(**img)
+    # decode the output
+    caption = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
+    return caption
+
+
+"""
+## Preprocessing Function
+"""
+
+def preprocess(items):
+  # preprocess the image
+  pixel_values = image_processor(items["image"], return_tensors="pt").pixel_values.to(device)
+  # tokenize the caption with truncation and padding
+  targets = tokenizer([ sentence["raw"] for sentence in items["sentences"] ], 
+                      max_length=max_length, padding="max_length", truncation=True, return_tensors="pt").to(device)
+  return {'pixel_values': pixel_values, 'labels': targets["input_ids"]}
+
+"""
+## Metric Evaluation
+"""
+
+# %%
+def compute_metrics(eval_pred):
+  preds = eval_pred.label_ids
+  labels = eval_pred.predictions
+  # decode the predictions and labels
+  pred_str = tokenizer.batch_decode(preds, skip_special_tokens=True)
+  labels_str = tokenizer.batch_decode(labels, skip_special_tokens=True)
+  # compute the rouge score
+  rouge_result = rouge.compute(predictions=pred_str, references=labels_str)
+  # multiply by 100 to get the same scale as the rouge score
+  rouge_result = {k: round(v * 100, 4) for k, v in rouge_result.items()}
+  # compute the bleu score
+  bleu_result = bleu.compute(predictions=pred_str, references=labels_str)
+  # get the length of the generated captions
+  generation_length = bleu_result["translation_length"]
+  return {
+        **rouge_result, 
+        "bleu": round(bleu_result["bleu"] * 100, 4), 
+        gen_len": bleu_result["translation_length"] / len(preds)
+  }
+
+# %%
+"""
+## Compare with other model
+"""
+
+# %%
+# using the pipeline API
+image_captioner = pipeline("image-to-text", model="Abdou/vit-swin-base-224-gpt2-image-captioning")
+image_captioner.model = image_captioner.model.to(device)
+
+# %%
+get_evaluation_metrics(image_captioner.model, test_dataset)
+
