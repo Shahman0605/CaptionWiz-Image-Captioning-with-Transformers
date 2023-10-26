346df4a0c7b2ce1283b07ecb1b00710e4fa6b7e2
Manan Shah <105140673+Shahman0605@users.noreply.github.com>
2023-10-26 18:38:30 +0530

    Add files via upload

---
+++ Models/Models.py
@@ -0,0 +1,17 @@
+"""
+## MODELS
+"""
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+import urllib.parse as parse
+import os
+
+# %%
+# the encoder model that process the image and return the image features
+# encoder_model = "WinKawaks/vit-small-patch16-224"
+# encoder_model = "google/vit-base-patch16-224"
+# encoder_model = "google/vit-base-patch16-224-in21k"
+encoder_model = "microsoft/swin-base-patch4-window7-224-in22k"
+# the decoder model that process the image features and generate the caption text
+# decoder_model = "bert-base-uncased"
+# decoder_model = "prajjwal1/bert-tiny"
+decoder_model = "gpt2"
